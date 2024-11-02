2fe7724c82494a5d6d0c8b7450cab0dd2a98ac8a
Monica.G.Bidart <monica.bidart@gmail.com>
2024-11-02 02:41:27 -0300

    Dia 1

---
+++ Dia-1/ejercicio_codificacion_3.py
@@ -0,0 +1,6 @@
+print('Práctica Print 3')
+'''
+Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, 
+sino como resultado de una operación matemática
+'''
+print(500 + 50 + 5)
