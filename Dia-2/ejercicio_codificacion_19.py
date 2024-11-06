8b862fbb1aa672c570ae74b559baa55151cfe5ec
Monica.G.Bidart <monica.bidart@gmail.com>
2024-11-06 13:45:18 -0300

    Add Ejercicios Práctica Formatear Cadenas

---
+++ Dia-2/ejercicio_codificacion_19.py
@@ -0,0 +1,9 @@
+print('Práctica Formatear Cadenas 1')
+'''
+Necesitamos imprimir el nombre y número de asociado dentro de la siguiente frase:
+Estimado/a (nombre_asociado), su número de asociado es: (numero_asociado)
+'''
+
+nombre_asociado = 'Jesse Pinkman'
+numero_asociado = 399058
+print(f'Estimado/a {nombre_asociado} su número de asociado es: {numero_asociado}')
---
+++ Dia-2/ejercicio_codificacion_21.py
@@ -0,0 +1,11 @@
+print('Práctica Formatear Cadenas 3')
+'''
+Muestra al usuario la cantidad de puntos acumulados dentro de la siguiente frase:
+Has ganado (puntos_nuevos) puntos! En total, acumulas (puntos_totales) puntos
+En esta ocasión, la cantidad de puntos acumulados (totales) será igual a los puntos_anteriores más los puntos_nuevos.
+'''
+
+puntos_anteriores = 875
+puntos_nuevos = 350
+puntos_totales = puntos_anteriores + puntos_nuevos
+print(f'Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos')
