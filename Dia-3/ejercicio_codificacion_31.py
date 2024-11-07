a2194a9af9e26adc00cd162aae50be3e4aea5dd3
Monica.G.Bidart <monica.bidart@gmail.com>
2024-11-07 12:22:10 -0300

    Add Clase Fragmentar-Slicing + Ejercicios Pŕactica Sub-String

---
+++ Dia-3/ejercicio_codificacion_31.py
@@ -0,0 +1,9 @@
+print('Práctica Sub-Strings 1')
+'''
+Extrae la primera palabra de la siguiente frase utilizando slicing, y muéstrala en pantalla:
+"Controlar la complejidad es la esencia de la programación"
+Pista: "Controlar" tiene un largo de 9 caracteres.
+'''
+frase = "Controlar la complejidad es la esencia de la programación"
+fragmento = frase[:9]
+print(fragmento)
---
+++ Dia-3/ejercicio_codificacion_32.py
@@ -0,0 +1,8 @@
+print('Práctica Sub-Strings 2')
+'''
+Toma cada tercer caracter empezando desde el noveno hasta el final de la frase, e imprime el resultado.
+"Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+'''
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+fragmento = frase[8::3]
+print(fragmento) 
