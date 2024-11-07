3aa27abea58a0830617533d50abf2d52801b0542
Monica.G.Bidart <monica.bidart@gmail.com>
2024-11-06 23:34:51 -0300

    Clase Redondeo

---
+++ Dia-2/redondear.py
@@ -0,0 +1,24 @@
+print(round(90 / 7))
+
+resultado = round(90 / 7)
+print(resultado)
+
+resultado_2 = 90 / 7
+redondeo = round(resultado)
+print(redondeo)
+
+print('Otro Ejemplo')
+valor = round(95.666666666666)
+valor_1 = 95.666666666666
+valor_2 = round(95.666666666666, 2)
+
+# Lo redondeamos dentro de la instancia de la variable
+print(valor)
+print(type(valor))
+
+# Lo redondeamos en la instancia de la impresión
+print(round(valor_1))
+print(type(valor_1))
+
+print(valor_2)
+print(type(valor_2))
