c7364677a868915435dfde81b3090f1052391e16
Monica.G.Bidart <monica.bidart@gmail.com>
2024-11-08 18:04:38 -0300

    Update Proyecto Día 3 Analizador de Texto Python

--- Dia-3/proyecto_dia_3.py
+++ Dia-3/proyecto_dia_3.py
@@ -1,43 +1,38 @@
 print('ANALIZADOR DE TEXTO - PYTHON')
-
 texto = input('Ingrese el texto a analizar\n').lower()
-letras = input('Ingrese 3 letras separadas por coma o espacios\n').lower()
-print(type(texto))
-print(type(letras))
-print(letras)
-letras = letras.split(',')
+letras = []
 
+letras.append(input('Ingrese la primer letra\n').lower())
+letras.append(input('Ingrese la segunda letra\n').lower())
+letras.append(input('Ingrese la tercer letra\n').lower())
 
 print('COMENZAMOS CON EL ANÁLISIS...')
-print(letras)
-print(texto)
 
-print('\nANÁLISIS I:\nEsta es la cantidad de veces que aparecen tus letras seleccionadas en el texto:')
+print('\nI - Esta es la cantidad de veces que aparecen tus letras en el texto:')
 letra_1 = texto.count(letras[0])
 letra_2 = texto.count(letras[1])
 letra_3 = texto.count(letras[2])
-print(f'Cantidades:\n{letras[0]} = {letra_1}\n{letras[1]} = {letra_2}\n{letras[2]} = {letra_3}\n')
+print(f'\n"{letras[0].upper()}" = {letra_1} veces\n"{letras[1].upper()}" = {letra_2} veces\n"{letras[2].upper()}" = {letra_3} veces')
 
-print('\nANÁLISIS II CUÁNTAS PALABRAS CONTIENE EL TEXTO INGRESADO')
-texto_en_palabras = texto.split(' ')
+print('\nII - CUÁNTAS PALABRAS CONTIENE EL TEXTO INGRESADO')
+texto_en_palabras = texto.split()# si no pongo ' ' igual las separa por espacios
 print(f'El texto ingresado contiene:\n{len(texto_en_palabras)} palabras')
-print(texto_en_palabras)
 
-print('\nANÁLISIS III INFORMAR LA PRIMERA Y LA ÚLTIMA LETRA DEL TEXTO INGRESADO')
-print(f'La Primera letra es: {texto[0].upper()} ')
-print(f'La Última letra es: {texto[-1].upper()}')
 
-print('\nANÁLISIS IV INVERTIMOS EL ORDEN DEL TEXTO INGRESADO')
+print('\nIII - INFORMAR LA PRIMERA Y LA ÚLTIMA LETRA DEL TEXTO INGRESADO')
+print(f'La Primera letra es: {texto[0].upper()}')
+print(f'La Última letra es: {texto[-1]}')
+
+print('\nIV - INVERTIMOS EL ORDEN DEL TEXTO INGRESADO')
 texto_invertido = texto_en_palabras
 texto_invertido.reverse()
-print(texto_invertido)
 
 lista_a_texto = ' '.join(texto_invertido)
 print(lista_a_texto)
 
-print('\nANÁLISIS V INFORMAMOS SI EXISTE LA PALABRA "PYTHON" EN EL TEXTO INGRESADO')
+print('\nV - INFORMAMOS SI EXISTE LA PALABRA "PYTHON" EN EL TEXTO INGRESADO')
 diccionario_existe = {True: 'CONTIENE', False: 'NO CONTIENE'}
 existe = str(diccionario_existe['python' in texto])
-cadena = f'El texto ingresado {existe} la palabra "PYTHON"'
+cadena = f'El texto ingresado {existe} la palabra "PYTHON"\n\n'
 print(cadena)
 
